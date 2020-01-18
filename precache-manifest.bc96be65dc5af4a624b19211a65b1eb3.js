7520a54b790dff2f00967126202b7c8cd61ecd7b
hsins <hsinspeng@gmail.com>
2020-01-18 19:39:14 +0800

    Updates

---
+++ precache-manifest.bc96be65dc5af4a624b19211a65b1eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0792c044d5fd35a5c2a4c1b9b7bb668",
+    "url": "/udemy_React-Dice/index.html"
+  },
+  {
+    "revision": "e67808cc535bfe692b18",
+    "url": "/udemy_React-Dice/static/css/main.83d12e55.chunk.css"
+  },
+  {
+    "revision": "8b8869494fcbd7ab01bb",
+    "url": "/udemy_React-Dice/static/js/2.0073e38c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/udemy_React-Dice/static/js/2.0073e38c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e67808cc535bfe692b18",
+    "url": "/udemy_React-Dice/static/js/main.5d433c84.chunk.js"
+  },
+  {
+    "revision": "7ca80f9e806876f12a66",
+    "url": "/udemy_React-Dice/static/js/runtime-main.5f588a5f.js"
+  }
+]);
